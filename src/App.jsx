ae79be028aa10bf2df0725952a4b1dce57f1b7d4
Takezo Shinmen <xs2takezo@gmail.com>
2026-02-27 02:01:45 +0500

    HomeUpdate

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Blur from "./blur";
+import Home from "./Home";  // Remove .jsx extension (optional but cleaner)
 import Research from "./research";
 import NotFound from "./NotFound";
-
-function Home() {
-  return <h1>Home Page</h1>;
-}
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home />} />  {/* Now uses the imported Home */}
       <Route path="/blur" element={<Blur />} />
       <Route path="/research" element={<Research />} />
 
