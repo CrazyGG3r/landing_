fff1e738e5ba77678a99eee9d8a67ab709ae8bcd
CrazyGG3r <54592666+CrazyGG3r@users.noreply.github.com>
2026-09-05 16:26:31 +0000

    chore(admin): publish ProjectZaman 0b61d33

--- public/admin/postscript-c6fabe90.js
+++ public/admin/postscript-c6fabe90.js
@@ -5,7 +5,7 @@ await Promise.all([
   import("./static/scripts/script-3-947bf5c1.js"),
   import("./static/scripts/script-4-cc8afa9d.js"),
   import("./static/scripts/script-5-7e107d7e.js"),
-  import("./static/scripts/script-6-1db9afde.js"),
+  import("./static/scripts/script-6-8b95b0c5.js"),
   import("./static/scripts/script-7-5ef560a8.js"),
   import("./static/scripts/script-8-632b2822.js"),
   import("./static/scripts/script-9-be15df42.js")
